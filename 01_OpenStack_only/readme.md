5f968cd9210892e418e497ee49b265ac3a464cd3
AlvaroRuizDelgado <alvaro@Rui.local>
2017-08-21 17:34:21 +0900

    First commit

---
+++ 01_OpenStack_only/readme.md
@@ -0,0 +1,6 @@
+
+
+Expanding on Solinea's Spencer Smith's blog.
+https://rsmitty.github.io/Terraform-Ansible-Kubernetes/
+
+
---
+++ 01_OpenStack_only/vars.tf
@@ -0,0 +1,7 @@
+node-count="2"
+internal-ip-pool="net-private"
+floating-ip-pool="public"
+image-name="Ubuntu-14.04LTS"
+image-flavor="m1.small"
+security-groups="default,k8s-sg"
+key-pair="mykey"
