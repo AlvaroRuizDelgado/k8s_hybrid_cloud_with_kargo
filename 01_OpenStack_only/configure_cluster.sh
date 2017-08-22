7b2869ead2dc60e44fe4348bf5fd7d672d711bae
AlvaroRuizDelgado <alvaro.ruiz.delgado@gmail.com>
2017-08-22 23:22:50 +0900

    Small edits

--- 01_OpenStack_only/configure_cluster.sh
+++ 01_OpenStack_only/configure_cluster.sh
@@ -3,19 +3,24 @@
 # You need kubectl installed beforehand.
 # $1: IP address of the master machine
 
+# kubectl config set-credentials k8sdemo/openstack --username=centos --client-key=~/.ssh/id_rsa
+# kubectl config set-cluster k8sdemo/openstack --server https://$1:6443
+# kubectl config set-context k8sdemo/openstack --user=k8sdemo/openstack --cluster=k8sdemo/openstack
+# kubectl config use-context k8sdemo/openstack
+
 mkdir k8s_credentials
 ssh centos@$1 sudo cat /etc/kubernetes/ssl/ca.pem > k8s_credentials/ca.pem
 ssh centos@$1 sudo cat /etc/kubernetes/ssl/admin-k8s-master-key.pem > k8s_credentials/admin-k8s-master-key.pem
 ssh centos@$1 sudo cat /etc/kubernetes/ssl/admin-k8s-master.pem > k8s_credentials/admin-k8s-master.pem
 
 kubectl config set-cluster k8sdemo/openstack --server=https://$1:6443 \
-    --certificate-authority=k8s_credentials/ca.pem
+            --certificate-authority=k8s_credentials/ca.pem
 
 kubectl config set-credentials k8sdemo/openstack \
-    --username=centos --client-key=~/.ssh/id_rsa \
-    --certificate-authority=k8s_credentials/ca.pem \
-    --client-key=k8s_credentials/admin-k8s-master-key.pem \
-    --client-certificate=k8s_credentials/admin-k8s-master.pem
+            --username=centos --client-key=~/.ssh/id_rsa \
+            --certificate-authority=k8s_credentials/ca.pem \
+            --client-key=k8s_credentials/admin-k8s-master-key.pem \
+            --client-certificate=k8s_credentials/admin-k8s-master.pem
 
 kubectl config set-context k8sdemo/openstack --cluster=k8sdemo/openstack --user=k8sdemo/openstack
 kubectl config use-context k8sdemo/openstack
--- readme.md
+++ readme.md
@@ -8,3 +8,11 @@ https://rsmitty.github.io/Terraform-Ansible-Kubernetes/
 
 The current kargo kubectl version gives warnings on duplicate proto types registered, it's better to download the official version.
 https://kubernetes.io/docs/tasks/tools/install-kubectl/#install-kubectl-binary-via-curl
+
+## Kubectl configuration
+
+The configuration is not done automatically.
+https://github.com/kubernetes-incubator/kubespray/issues/257
+
+Possible solution:
+https://github.com/CiscoCloud/kubernetes-ansible/issues/105
